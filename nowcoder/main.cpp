ca5b8e3e4f8a9d5128a1f083761c24ab324263e8
yex <317390005@qq.com>
2024-07-01 22:14:12 +0800

    lgy

---
+++ nowcoder/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <array>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <complex>
+#include <cmath>
+#include <numeric>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <ctime>
+
+using namespace std;
+#define ios ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+
+int main() {
+    return 0;
+}
