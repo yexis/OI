7879bacf90e011eeb4b28dd315cdd0e094b8afb9
yex <317390005@qq.com>
2024-09-23 22:09:55 +0800

    牛客周赛59 f.

--- nowcoder/牛客周赛59/f.cpp
+++ nowcoder/牛客周赛59/f.cpp
@@ -29,82 +29,65 @@
 #include <random>
 #include <ctime>
 #include <limits>
-#include <climits>
 
 using namespace std;
 #define ios ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 
-/*
- * 
-*/
-
 using ll = long long;
 using ull = unsigned long long;
 using pii = pair<int, int>;
-using pli = pair<ll, int>;
-using pil = pair<int, ll>;
-using pll = pair<ll, ll>;
-using puu = pair<ull, ull>;
-const int dir[4][2] = {{-1, 0},
-                       {1,  0},
-                       {0,  -1},
-                       {0,  1}};
-const int INF = 0x3f3f3f3f;
 const int mod = 1e9 + 7;
-const string YES = "YES";
-const string NO = "NO";
-
-ll power(ll x, ll b) {
-    ll ans = 1;
-    while (b) {
-        if (b & 1) {
-            ans *= x;
-            ans %= mod;
+struct Com {
+    constexpr static int maxn = 4010;
+    ll fac[maxn], inv[maxn];
+    Com() {
+        memset(fac, 0, sizeof(fac));
+        memset(inv, 0, sizeof(inv));
+        fac[0] = inv[0] = 1;
+        for (int i = 1; i < maxn; i++) {
+            fac[i] = fac[i - 1] * i % mod;
+            inv[i] = power(fac[i], mod - 2);
         }
-        x *= x;
-        x %= mod;
-        b >>= 1;
     }
-    return ans;
-}
-
-void solve(vector<int>& a) {
-    int n = a.size();
-    ll dp[n][n];
-    memset(dp, 0, sizeof(dp));
-
-    for (int i = 1; i < n; i++) {
-        for (int j = i - 1; j >= 0; j--) {
-            if (a[j] == a[i]) {
-                if (i - j + 1 == 2) {
-                    dp[j][i] = 0;
-                } else {
-                    // > 2
-                    dp[j][i] = 2 * dp[j + 1][i - 1] % mod;
-                }
-            } else if (a[i] != a[j]) {
-                if (i - j + 1 == 2) {
-                    dp[j][i] = 1;
-                } else {
-                    dp[j][i] += 2 * dp[j + 1][i - 1];
-                    dp[j][i] %= mod;
-                    dp[j][i] += power(2, i - j - 1);
-                    dp[j][i] %= mod;
-                }
+    ll power(ll x, ll b) {
+        ll ans = 1;
+        while (b) {
+            if (b & 1) {
+                ans *= x;
+                ans %= mod;
             }
+            x *= x;
+            x %= mod;
+            b >>= 1;
         }
+        return ans;
     }
-    cout << dp[0][n - 1] << "\n";
-    return;
-}
+    ll get(int n, int m) {
+        if (n < 0 || m < 0 || n < m) {
+            return 0;
+        }
+        return fac[n] * inv[m] % mod * inv[n - m] % mod;
+    }
+};
 
 int main() {
     int n;
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; i++) {
-        cin  >> a[i];
+        cin >> a[i];
     }
-    solve(a);
+    // 范德蒙德卷积
+    Com c;
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] != a[j]) {
+                ans += c.power(2, j - i - 1) * c.get(n + i - j - 1, i);
+                ans %= mod;
+            }
+        }
+    }
+    cout << ans << "\n";
     return 0;
 }
