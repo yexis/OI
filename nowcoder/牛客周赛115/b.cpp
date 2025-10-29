0581dbddbe19680cdd17679d3c159abc067849c1
liguoyang <317390005@qq.com>
2025-10-29 22:54:47 +0800

    123

---
+++ nowcoder/牛客周赛115/b.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <array>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <complex>
+#include <cmath>
+#include <numeric>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <ctime>
+#include <limits>
+#include <climits>
+
+using namespace std;
+#define ios ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define next_per next_permutation
+#define call(x) (x).begin(), (x).end()
+#define debug(x) cout << (#x) << " = " << (x) << endl;
+#define debugout(x) cout << (#x) << " = " << (x) << endl;
+#define debugerr(x) cerr << (#x) << " = " << (x) << endl;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pil = pair<int, ll>;
+using pll = pair<ll, ll>;
+using pbi = pair<bool, int>;
+using pib = pair<int, bool>;
+using pis = pair<int, string>;
+using psi = pair<string, int>;
+using puu = pair<ull, ull>;
+using arr = array<int, 3>;
+using arr3 = array<int, 3>;
+using arr4 = array<int, 4>;
+using arr5 = array<int, 5>;
+
+const int dir[4][2] = {{-1, 0},
+                       {1,  0},
+                       {0,  -1},
+                       {0,  1}};
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const int mod = 1e9 + 7;
+const string YES = "YES";
+const string NO = "NO";
+
+ll power(ll x, ll b, ll m = mod) {
+    ll ans = 1;
+    while (b) {
+        if (b & 1) {
+            ans *= x;
+            ans %= m;
+        }
+        x *= x;
+        x %= m;
+        b >>= 1;
+    }
+    return ans;
+}
+
+/*
+ * 
+*/
+
+/*
+ * 1 2 3 4 5 6 7 8 9 10 11 12 
+ * 2 4 6 8 10 12
+ * 4 8 12 
+ * 8 16 32
+ * 16
+ * 
+*/
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    int b = power(2, k);
+    cout << b * n << "\n";
+}
+int main() {
+    ios;
+    cout << fixed << setprecision(20);
+
+    int T = 1; 
+    cin >> T;
+    while (T--) {
+    	solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
